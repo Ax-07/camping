8c5d898851c8699029f3a639a8622c4be200c473
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-20 12:19:11 +0100

    modif chemin d'acces

--- services/api/chaletApi.js
+++ services/api/chaletApi.js
@@ -1,6 +1,6 @@
 export const getDataChalets = async () => {
     try {
-        const response = await fetch('../../db/descriptifChalet.json');
+        const response = await fetch('../db/descriptifChalet.json');
         const data = await response.json();
         return data;
     } catch (error) {
