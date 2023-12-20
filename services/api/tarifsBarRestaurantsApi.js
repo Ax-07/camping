d2827111bf95e4b765eaa59243d7654d466829b5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-20 12:24:17 +0100

    modif chemin d'acces

--- services/api/tarifsBarRestaurantsApi.js
+++ services/api/tarifsBarRestaurantsApi.js
@@ -1,6 +1,6 @@
 export const getTarifsBarRestaurants = async () => {
     try {
-        const response = await fetch('/db/carteComplete.json');
+        const response = await fetch('./db/carteComplete.json');
         const data = await response.json();
         return data;
     } catch (error) {
